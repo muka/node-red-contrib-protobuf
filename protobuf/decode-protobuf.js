d85c0c77cb4fbe8438fc317a0734137bdf94dd6d
Zdzisław Krajewski <zdzichucb@gmail.com>
2017-03-11 16:02:32 +0100

    Working with latest version of protobufjs

--- protobuf/decode-protobuf.js
+++ protobuf/decode-protobuf.js
@@ -1,46 +1,67 @@
 
 module.exports = function(RED) {
 
-    function DecodeProtoBufNode(config) {
-        RED.nodes.createNode(this,config);
-        var node = this;
+  function DecodeProtoBufNode(config) {
+    RED.nodes.createNode(this,config);
+    var node = this;
 
 	// Extract configuration settings
 
-	if(config.protofile.length == 0)
-	  throw ".proto file is not defined";
+  	if(config.protofile.length == 0)
+  	  throw ".proto file is not defined";
 
-	node.protofile = config.protofile;
+  	node.protofile = config.protofile;
 
-	if(config.messagetype.length == 0)
-	  throw "message type is not defined";
+  	if(config.messagetype.length == 0)
+  	  throw "message type is not defined";
 
-	node.messagetype = config.messagetype;
+  	node.messagetype = config.messagetype;
 
-	// Create protocol buffers builder for decode
+  	// Create protocol buffers builder for decode
 
-        var ProtoBuf = require("protobufjs");
-        var builder = ProtoBuf.loadProtoFile(node.protofile);
+    var ProtoBuf = require("protobufjs");
+    // var builder = ProtoBuf.loadProtoFile(node.protofile);
+    var root = ProtoBuf.loadSync(node.protofile);
+    if (root == null){
+      node.error("Unable to load proto file");
+      node.status({fill:"red",shape:"ring",text:"Unable to load proto file"});
+      throw "Unable to load proto file";
+    }
 
-	// Create the appropriate message object for the decode
+    // Create the appropriate message object for the decode
 
-        var msgTemplate = builder.build(node.messagetype);
+    // var msgTemplate = builder.build(node.messagetype);
+    var MessageTemplate = root.lookup(node.messagetype)
+    if (root == null){
+      node.error("Unable to load message");
+      node.status({fill:"red",shape:"ring",text:"Unable to load message"});
+      throw "Unable to load message";
+    }
 
-        this.on('input', function(msg) {
+    node.status({});
+    this.on('input', function(msg) {
 
-            // Decode the incoming bytes to the appropriate message object
-		
-            var mymsg = msgTemplate.decode(msg.payload);
-
+      // Decode the incoming bytes to the appropriate message object
+      var reader = ProtoBuf.Reader.create(msg.payload);
+      if (reader == null){
+        node.error("Unable to create Reader from buffer.");
+        node.status({fill:"red",shape:"ring",text:"Unable to create Reader from buffer."});
+        throw "Unable to create Reader from buffer.";
+      }
+      var mymsg = MessageTemplate.decode(reader);
+      if (mymsg == null){
+        node.error("Unable to decide buffer.");
+        node.status({fill:"red",shape:"ring",text:"Unable to decide buffer."});
+        throw "Unable to decide buffer.";
+      }
 	    // Modify the payload to contain the decoded object
 
-	    msg.payload = mymsg;
+	    msg.payload = mymsg.toJSON();
 
 	    // Pass it along
 
-            node.send(msg);
-        });
-    }
+      node.send(msg);
+    });
+  }
     RED.nodes.registerType("decode-protobuf",DecodeProtoBufNode);
 }
-
--- protobuf/encode-protobuf.js
+++ protobuf/encode-protobuf.js
@@ -20,27 +20,62 @@ module.exports = function(RED) {
 	// Create protocol buffers builder for encode
         var ProtoBuf = require("protobufjs");
 	// Create the configured message type
-        var builder = ProtoBuf.loadProtoFile(node.protofile),
-		Message = builder.build(node.messagetype);
+        // var builder = ProtoBuf.loadProtoFile(node.protofile),
+		// Message = builder.build(node.messagetype);
+  var root = ProtoBuf.loadSync(node.protofile)
+  if (root == null){
+    node.error("Unable to load proto file");
+    node.status({fill:"red",shape:"ring",text:"Unable to load proto file"});
+    throw "Unable to load proto file";
+  }
+	var MessageTemplate = root.lookup(node.messagetype)
+  if (root == null){
+    node.error("Unable to load message");
+    node.status({fill:"red",shape:"ring",text:"Unable to load message"});
+    throw "Unable to load message";
+  }
+  if (MessageTemplate == null){
+    node.error("Message not found");
+    node.status({fill:"red",shape:"ring",text:"Message not found"});
+    throw "something is fucked up";
+  }
 
-        this.on('input', function(msg) {
+  node.status({});
+  this.on('input', function(msg) {
 
 	    // Create an instance of the message from payload containing
             // JSON format data
-	    var message = new Message( msg.payload );
-	   
-	    // Encode the message
-	    var byteBuffer = message.encode();
+	    // var message = new Message();
+      var message = MessageTemplate.create( msg.payload )
+      if (message == null){
+        node.error("Unable to create message");
+        node.status({fill:"red",shape:"ring",text:"Unable to create message"});
+        throw "something is fucked up";
+      }
 
-	    var buffer = byteBuffer.toBuffer();
-	    
+      var verified = MessageTemplate.verify(message);
+      if (verified != null){
+        node.error(verified);
+        node.status({fill:"red",shape:"ring",text:verified});
+        throw "something is fucked up " + verified;
+      }
+
+	    // Encode the message
+	    var buffer = MessageTemplate.encode(message).finish();
+      if (buffer == null){
+        node.error("Unable to create buffer");
+        node.status({fill:"red",shape:"ring",text:"Unable to create buffer"});
+        throw "something is fucked up buffer";
+      }
+	    // var buffer = writer.finish();
+
 	    // Modify the payload to contain the encoded object
 	    msg.payload = buffer;
 
 	    // Pass it along
+           node.status({});
             node.send(msg);
         });
     }
     RED.nodes.registerType("encode-protobuf",EncodeProtoBufNode);
 }
-
